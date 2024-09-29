1393e61871dd8946b47543e123900080a02180bc
Fabys <fabian@pfeiffer.tirol>
2024-09-29 13:44:05 +0000

    disable ts_ls formatting

--- .nvim/nvim/lua/plugins/lsp.lua
+++ .nvim/nvim/lua/plugins/lsp.lua
@@ -155,7 +155,12 @@ return {
           })
         end,
       },
-      ts_ls = {},
+      ts_ls = {
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+      },
       html = { filetypes = { 'html' } },
       cssls = {},
       tailwindcss = {},
