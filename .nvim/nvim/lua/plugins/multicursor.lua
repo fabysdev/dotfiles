dd54f4599d7edd0549cd785db4cdd6e1f125ff64
Fabys <fabian@pfeiffer.tirol>
2024-10-05 08:50:28 +0000

    change multicursor plugin

--- .nvim/nvim/lua/plugins/multicursor.lua
+++ .nvim/nvim/lua/plugins/multicursor.lua
@@ -1,29 +1,11 @@
 return {
-  'smoka7/multicursors.nvim',
-  event = 'VeryLazy',
-  dependencies = {
-    'nvimtools/hydra.nvim',
-  },
-  opts = {},
-  cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-  keys = {
-    {
-      mode = { 'v', 'n' },
-      '<Leader>m',
-      '<cmd>MCstart<cr>',
-      desc = 'Create a selection for selected text or word under the cursor',
-    },
-    {
-      mode = { 'v', 'n' },
-      '<Leader>mv',
-      '<cmd>MCvisual<cr>',
-      desc = 'Select the last visual mode selection and start listening for the actions',
-    },
-    {
-      mode = { 'v', 'n' },
-      '<Leader>mp',
-      '<cmd>MCpattern<cr>',
-      desc = 'Select the last visual mode selection and start listening for the actions',
-    },
-  },
+  'mg979/vim-visual-multi',
+  config = function()
+    vim.g.VM_maps = {
+      ['Find Next'] = '<A-s>',
+      ['Goto Prev'] = '{',
+      ['Goto Next'] = '}',
+      ['Dot'] = '#',
+    }
+  end,
 }
